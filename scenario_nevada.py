fec37060f58bf270f92c325232443873e07239db
Nikolai V. Chr <emptyyetfull@gmail.com>
2021-12-28 14:24:57 +0100

    Another column added to Nevada

--- scenario_nevada.py
+++ scenario_nevada.py
@@ -26,6 +26,22 @@ def build_scenario(path: str) -> ScenarioContainer:
         StaticTarget(mpt.MPTarget.BRADLEY_TANK, g.Position(-115.7405, 37.4832, 2307.9216), 162.6, 0),
         StaticTarget(mpt.MPTarget.MLRS_ROCKET_LAUNCHER, g.Position(-115.7258, 37.3511, 1990.4022), 202.6, 0),
         StaticTarget(mpt.MPTarget.M1_TANK, g.Position(-115.7330, 37.0683, 1801.5064), 173.5, 0),
+
+        # Column SSW of KXTA
+        StaticTarget(mpt.MPTarget.TOWER, g.Position(-115.6820, 36.5998, 946.1065), 324.8, 0),
+        StaticTarget(mpt.MPTarget.M1_TANK, g.Position(-115.6925, 36.6152, 946.7155), 345.4, 0),
+        StaticTarget(mpt.MPTarget.M1_TANK, g.Position(-115.6959, 36.6422, 941.4788), 358.6, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.6959, 36.6436, 942.6909), 358.6, 0),
+        StaticTarget(mpt.MPTarget.MLRS_ROCKET_LAUNCHER, g.Position(-115.6960, 36.6454, 944.1739), 358.7, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.6871, 36.7281, 982.4791), 348.0, 0),
+        StaticTarget(mpt.MPTarget.STRYKER_TANK, g.Position(-115.7221, 36.7789, 1426.4818), 327.8, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.7097, 36.8296, 1908.9063),  31.1, 0),
+        StaticTarget(mpt.MPTarget.BRADLEY_TANK, g.Position(-115.6989, 36.8735, 1443.0649), 353.9, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.7075, 36.9562, 1356.6058),  17.4, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.6894, 37.0156, 1619.5441), 342.9, 0),
+        StaticTarget(mpt.MPTarget.BUFFALO_MINE, g.Position(-115.6897, 37.0164, 1619.0213), 342.5, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.6907, 37.0189, 1629.7599), 341.4, 0),
+        StaticTarget(mpt.MPTarget.TRUCK, g.Position(-115.7267, 37.0925, 1557.7902), 340.3, 0),
     ]
 
     scenario = ScenarioContainer('nevada', 'Nevada',  'Cf. http://opredflag.com/forum_threads/3154248',
