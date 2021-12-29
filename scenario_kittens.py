84a6de6dc6d1242b4c2263612388a89a5bc2be52
Nikolai V. Chr <emptyyetfull@gmail.com>
2021-12-29 20:25:20 +0100

    Scenario for event 29th Dec 2021

---
+++ scenario_kittens.py
@@ -0,0 +1,17 @@
+import hunter.geometry as g
+import hunter.mp_targets as mpt
+from hunter.scenarios import (ScenarioContainer, StaticTarget)
+
+
+def build_scenario(path: str) -> ScenarioContainer:
+    static_targets = [
+        # Event
+        StaticTarget(mpt.MPTarget.TOWER, g.Position(-116.321, 33.259, 159),  91.4, 0),#L08
+        StaticTarget(mpt.MPTarget.TOWER, g.Position(-118.439, 35.135, 1207), 324.8, 0),#KLSP
+    ]
+
+    scenario = ScenarioContainer('kittens', 'Kittens',  'Cf. http://opredflag.com/forum_threads/3154248',
+                                 (-116., 35), (-115., 38),
+                                 'KXTA', None, 60)
+    scenario.add_static_targets(static_targets)
+    return scenario
